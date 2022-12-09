0d6a541b60736e19644610727f9ae21c435ac40f
Zan Preston <zan.preston@banno.com>
2022-12-09 15:27:08 -0600

    Continuing Streaming Solutions...

--- day7/day7.scala
+++ day7/day7.scala
@@ -5,7 +5,7 @@
 
 import cats.effect.{IO, IOApp}
 import cats.effect.std.Console
-import fs2.{Stream, text}
+import fs2.{Stream, Pipe, Pull, text}
 import fs2.io.file.{Files, Path}
 
 
@@ -20,8 +20,8 @@ object ConsoleStream extends IOApp.Simple {
   final case class DirSize(name: String, size: Int)
   final case class State(sizes: List[DirSize])
 
-  def parse(line: String) : Line = {
-    line.split("""\s""") match {
+  def parse(in: String) : Line = {
+    in.split("""\s""") match {
       case Array("$", "cd", name) => CD(name)
       case Array("$", "ls") => LS
       case Array("dir", name) => Directory(name)
@@ -29,6 +29,24 @@ object ConsoleStream extends IOApp.Simple {
     }
   }
 
+  val dirInterpreter: Pipe[IO, Line, DirSize] = { (in : Stream[IO, Line]) => 
+
+    def pull(in: Stream[IO, Line], state: State): Pull[IO, DirSize, Unit] = {
+      in.pull.uncons1.flatMap{
+        case Some((line, rest)) => line match {
+          case _ => Pull.output1(DirSize("some item", 10)).flatMap(_ => pull(rest, state))
+        }
+        case None => Pull.done
+      }
+      // for { 
+      //
+      //   // _ <- Pull.output1(DirSize("", 10))
+      // } yield ()
+    }
+
+    pull(in, State(List(DirSize("", 0)))).stream
+
+  }
 
 
   // val fileName: String = "day7-in.txt"
@@ -37,17 +55,18 @@ object ConsoleStream extends IOApp.Simple {
     Files[IO].readUtf8Lines(Path(fileName))
     .filter(_.nonEmpty)
     .map(parse)
-    .scan(State(List(DirSize("", 0))))((state, line: Line) => line match {
-      case CD("/") => State(List(DirSize("", 0)))
-      case CD("..") => state.sizes match {
-        case current :: parent :: rest => State(parent.copy(size = parent.size + current.size) +: rest)
-      }
-      case CD(name) => State(DirSize(name, 0) +: state.sizes)
-      case File(size, _) => state.sizes match {
-        case current :: rest => State(DirSize(current.name, current.size + size) +: rest)
-      }
-      case _ => state
-    } )
+    .through(dirInterpreter)
+    // .scan(State(List(DirSize("", 0))))((state, line: Line) => line match {
+    //   case CD("/") => State(List(DirSize("", 0)))
+    //   case CD("..") => state.sizes match {
+    //     case current :: parent :: rest => State(parent.copy(size = parent.size + current.size) +: rest)
+    //   }
+    //   case CD(name) => State(DirSize(name, 0) +: state.sizes)
+    //   case File(size, _) => state.sizes match {
+    //     case current :: rest => State(DirSize(current.name, current.size + size) +: rest)
+    //   }
+    //   case _ => state
+    // } )
     .foreach(Console[IO].println(_))
     .compile.drain
 
